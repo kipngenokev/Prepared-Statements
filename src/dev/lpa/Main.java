0d02b09269f05f1aab4861715a28049cd9d19b03
brian070069 <kkipngeno409@gmail.com>
2024-11-11 00:50:42 +0300

    first commit on prepared statements

---
+++ src/dev/lpa/Main.java
@@ -0,0 +1,64 @@
+package dev.lpa;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Main {
+
+    private static String ARTIST_INSERT =
+            "INSERT INTO music.artists (artist_name) VALUES (?)";
+    private static String ALBUM_INSERT =
+            "INSERT INTO music.albums (artist_id,album_name ) VALUES (? , ?)";
+    private static String SONG_INSERT =
+            "INSERT INTO music.songs (album_id,track_number,song_title) "+
+                    "VALUES (? ,? ,? )";
+    public static void main(String[] args) {
+        var dataSource = new MysqlDataSource();
+        dataSource.setServerName("localhost");
+        dataSource.setPort(3306);
+        dataSource.setDatabaseName("music");
+        try {
+            dataSource.setContinueBatchOnError(false);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        try(Connection connection = dataSource.getConnection(
+                System.getenv("MYSQLUSER"),
+                System.getenv("MYSQLPASS"))
+        ) {
+            String sql = "SELECT *FROM music.albumview WHERE artist_name =?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1,"Elf");
+            ResultSet resultSet = ps.executeQuery();
+            printRecords(resultSet);
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean printRecords(ResultSet resultSet) throws SQLException{
+        boolean foundData = false;
+
+        var meta = resultSet.getMetaData();
+        System.out.println("=============");
+
+        for(int i=1;i <= meta.getColumnCount();i++) {
+            System.out.printf("%-15s",meta.getColumnName(i).toUpperCase());
+        }
+        System.out.println();
+
+        while(resultSet.next()) {
+            for(int i=1;i <= meta.getColumnCount();i++) {
+                System.out.printf("%-15s",resultSet.getString(i));
+            }
+            System.out.println();
+            foundData = true;
+        }
+        return foundData;
+    }
+}
